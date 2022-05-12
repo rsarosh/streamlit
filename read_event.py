77987e56008feedd58748b6a9fbc30079cb63499
Rafat Sarosh <rsarosh@hotmail.com>
2022-05-11 18:52:41 -0700

    refectoring write_event

--- read_event.py
+++ read_event.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 # https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-python-how-to-use-queues
 
 load_dotenv('.env')
-# all types of EventGridEvents below produce same DeserializedEvent
+
 connection_str = os.environ['azure_servicebus_conection_str']
 queue_name = os.environ['azure_servicebus_queue_name']
 
@@ -18,7 +18,6 @@ if __name__ == '__main__':
             receiver = sb_client.get_queue_receiver(queue_name=queue_name)
             with receiver:
                 for message in receiver:
-
                     event = json.loads(str(message))
                     print(f"Event: {event['data']}")
                     receiver.complete_message(message)  # mark message as complete
--- write_event.py
+++ write_event.py
@@ -3,13 +3,17 @@ from dotenv import load_dotenv
 import os
 from azure.core.credentials import AzureKeyCredential
 from azure.eventgrid import EventGridPublisherClient, EventGridEvent
+import json
+import time
 
-event = EventGridEvent(
-    data={"team": "Analyst", "timestamp": datetime.datetime.now().isoformat()},
-    subject="Start Scrapping",
-    event_type="Scrapping",
-    data_version="1.0"
-)
+
+def create_data() -> str:
+    data = {}
+    data['producer'] = 'python app-1'
+    data['subscriber'] = 'python app-2'
+    data['timestamp'] = datetime.datetime.now().isoformat()
+    return json.dumps(data)
+
 
 load_dotenv('.env')
 
@@ -17,9 +21,24 @@ azure_event_key = os.environ['azure_event_key']
 azure_event_endpoint = os.environ['azure_event_endpoint']
 
 
-if __name__ == '__main__':
+def send_event(event):
     endpoint = azure_event_endpoint
     credential = AzureKeyCredential(azure_event_key)
     client = EventGridPublisherClient(endpoint, credential)
     client.send(event)
-    print("Event sent")
+    print(".")
+
+
+def create_event(event_data: str) -> EventGridEvent:
+    return EventGridEvent(
+        data=event_data,
+        subject="spider-1",
+        event_type="SpiderEvent",
+        data_version="1.0"
+    )
+
+
+if __name__ == '__main__':
+    while True:
+        send_event(create_event(create_data()))
+        time.sleep(4)  # sleep for 4 seconds
